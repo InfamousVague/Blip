3d1f8f23e86a17ef9516843147352c32000549e0
matt <matt@matts-MacBook-Air.local>
2026-04-03 16:44:29 -0400

    Remove website — moved to mattssoftware repo

--- website/src/main.tsx
+++
@@ -1,7 +0,0 @@
-import { createRoot } from "react-dom/client";
-import "./base-tokens.css";
-import "@blip/ui/tokens/tokens.css";
-import "./website.css";
-import { WebsiteApp } from "./WebsiteApp";
-
-createRoot(document.getElementById("root")!).render(<WebsiteApp />);
